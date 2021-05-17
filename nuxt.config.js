310b5e844882d84300e4903964eb1f6b7c68df94
Jahangir <devj@ip-172-26-12-177.ec2.internal>
2021-05-17 15:10:16 +0000

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,89 @@
+export default {
+  server: {
+    host: '0', // default: localhost
+    port: 3000
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'myapp',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next',
+  ],
+  auth: {
+        localStorage: false,
+        cookie: {
+            options: {
+                expires: 7,
+            },
+        },
+    strategies: {
+
+      local: {
+              scheme: 'refresh',
+        token: {
+          property: 'access'
+        },
+        refreshToken: {
+          property: 'refresh',
+          data: 'refresh',
+          maxAge: 60 * 60 * 24 * 30
+        },
+        user: {
+          property: false
+        },
+        endpoints: {
+          login: {
+            url: '/auth/jwt/verify/',
+            method: 'post',
+            propertyName: 'access',
+          },
+          refresh: {url: '/auth/jwt/refresh/', method: 'post', propertyName: 'refresh'},
+          logout: {},
+          user: {url: '/users/userdata/', method: 'get', propertyName: false}
+        }
+      },
+    }
+  },
+  axios: {
+    baseURL: 'http://172.26.0.134:8000'
+  },
+
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
